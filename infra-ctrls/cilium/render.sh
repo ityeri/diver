edada9bcb2e677776a008b5b407ddc59bb713e04
ityeri <ityeri9@gmail.com>
2026-07-04 22:11:27 +0900

    add: Add helm repo add & update line to cilium render.sh

--- infra-ctrls/cilium/render.sh
+++ infra-ctrls/cilium/render.sh
@@ -1,7 +1,11 @@
 rm ./infra-ctrls/cilium/raw-manifests.yaml
 
+helm repo add cilium https://helm.cilium.io/
+helm repo update cilium
+
 helm template cilium cilium/cilium \
   --namespace kube-system \
   --include-crds \
   -f ./infra-ctrls/cilium/values.yaml \
   > ./infra-ctrls/cilium/raw-manifests.yaml
+
